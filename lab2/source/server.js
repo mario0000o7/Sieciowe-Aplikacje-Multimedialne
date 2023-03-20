8085527a0c4c34c599754376412317610a08ec39
jancys <duchola1@gmail.com>
2023-03-20 14:54:58 +0100

    Add htmlFile

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -5,38 +5,42 @@ const app = express()
 let videoQuery
 let audioQuery
 let imgQuery
-function videoCancelFunction(res) {
-    videoQuery="cancel.mp4"
-    res.send(`<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
-    	<audio id="audioPlayer" controls src=${audioQuery}></audio>
-    	<br>
-    	<img id="posterImage" src=${imgQuery}>`
-    )
 
-}
-function audioCancelFunction(res) {
-    audioQuery="cancel.mp3"
-    res.send(`<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
-    	<audio id="audioPlayer" controls src=${audioQuery}></audio>
-    	<br>
-    	<img id="posterImage" src=${imgQuery}>`
-    )
-}
 app.get('/', (req, res) => {
     videoQuery = req.query.videoFile;
     audioQuery = req.query.audioFile;
     imgQuery = req.query.imgFile;
-    res.send(`<video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
+
+    let html = `
+<!DOCTYPE html>
+
+<script>
+    function videoCancelFunction() {
+    document.getElementById("videoPlayer").src = "cancel.mp4";
+
+    }
+    function audioCancelFunction() {
+        document.getElementById("audioPlayer").src = "cancel.mp3";
+    }
+</script>
+<html>
+<head>
+    <title>Lab2</title>
+</head>
+
+    <video id="videoPlayer" width="320" height="240" controls src=${videoQuery}></video>
         <br>
     	<audio id="audioPlayer" controls src=${audioQuery}></audio>
     	<br>
     	<img id="posterImage" src=${imgQuery}>
     	<br>
-        <button type="button" id="videoCancel" onclick="videoCancelFunction(res)">VideoCancel</button>
-        <button type="button" id="audioCancel" onclick="audioCancelFunction(res)">AudioCancel</button>
+        <button type="button" id="videoCancel" onclick="videoCancelFunction()">VideoCancel</button>
+        <button type="button" id="audioCancel" onclick="audioCancelFunction()">AudioCancel</button>
+</html>
 
 `
-    )
+res.send(html)
+
 })
 
 app.listen(4080)
